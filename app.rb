f2ebadbad94548f08f5896cbdf79d71b9569bdd1
Jonathan Luk <jluk87@gmail.com>
2018-10-17 20:54:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,15 +3,16 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
-  post '/newteam' do
-    puts params
-    erb :team
-  end
+
 
   get '/newteam' do
     erb :newteam
   end
 
+  post '/team' do
+    puts params
+    erb :team
+  end
 
 
 end
